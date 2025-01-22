4d776b465541e1adfcd42fb23ad7792a2f90cb05
samasaleh5 <ss2104@fayoum.edu.eg>
2025-01-22 21:28:33 +0200

    Add project files.

---
+++ Advanced-Assigment01/BubbleSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Assigment01
+{
+    internal class BubbleSort<T> where T : IComparable<T>
+    {
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+        //Optimization 1-Flag 2-last i elements already stored so we don't check them again
+        public static void OptimizedBubbleSort(T[] arr) 
+        { 
+            int size=arr.Length;
+            bool swapped;
+            for (int i = 0; i < size - 1; i++) 
+            {
+                swapped = false;
+                for (int j = 0; j < size - 1 - i; j++) 
+                {
+                    if (arr[j].CompareTo(arr[j+1]) > 0) 
+                    { 
+                        Swap(ref arr[j], ref arr[j+1]);
+                        swapped = true;
+
+                    }
+                }
+                if (!swapped)
+                    break;
+            }
+        }
+    }
+}
---
+++ Advanced-Assigment01/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Advanced_Assigment01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question1
+            int[] Array1= { 5, 1, 4, 2, 8 };
+            BubbleSort<int>.OptimizedBubbleSort(Array1);
+            foreach (int i in Array1) 
+            { 
+                Console.WriteLine(i);
+            }
+            #endregion
+        }
+    }
+}
